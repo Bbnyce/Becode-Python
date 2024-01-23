6faaceb70adf8cd7a9d660ad1a0c92077026d99a
Alice <alice@Cybersprint.myguest.virtualbox.org>
2024-01-23 13:44:16 +0100

    modification

---
+++ Python/test/drill-built-in-function.py
@@ -0,0 +1,8 @@
+# Creating a variable count_alpha
+count_alpha = len("Hello world!")
+
+# Printing the result
+print(count_alpha)
+
+# Creating a variable count_float
+count_float = float(count_alpha)
